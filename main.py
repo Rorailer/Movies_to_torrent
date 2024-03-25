af301cf1c3f7ad956bd6cf6f01d53d78e00bed54
Abdullah <152975734+Rorailer@users.noreply.github.com>
2024-03-26 02:21:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#!../apivenv/bin/python3
+
+from MyModules import *
+
+
+if __name__ == '__main__':
+    
+     
+    table = list_movies()
+    # print(table)
+    
+    selection = input("Enter index of movie to download: ") 
+    
+    info = search_by_id(table[int(selection)])
+    
+    url = torrent_url(info)
+    
+    name = info[0]['Title']
+    
+    
+    name = name_cleanup(name) + '.torrent'
+    
+    torrent_file(url, name )
+    
+    qbittorrent(name)
+    
+    subprocess.run('rm ~/Downloads/Torrents/' + name, shell=True)
+    
